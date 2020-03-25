f0fb12420a63ff54edaa996f938ae5badad9dd92
Ben Orenstein <ben.orenstein@gmail.com>
2020-03-25 17:40:45 -0400

    change stuff based on adam feedback

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  theme: {
+    maxWidth: {
+      xs: '20rem',
+      sm: '30rem',
+      md: '40rem',
+      lg: '50rem',
+      xl: '60rem',
+      '2xl': '70rem',
+      '3xl': '80rem',
+      '4xl': '90rem',
+      '5xl': '100rem',
+      full: '100%',
+      'online-users': '360px',
+    },
+
+    extend: {
+      fontSize: {
+        hero: '5rem'
+      },
+      height: {
+        'online-user-row': '69px',
+      },
+    },
+  }
+}
